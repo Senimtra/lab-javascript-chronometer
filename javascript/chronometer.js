3f4ad0ecb25da18cc4dabd4ad0dc752d93e9052e
Senimtra <senimtra@hotmail.com>
2021-06-28 16:43:21 +0200

    solve iteration 1: the logic

--- javascript/chronometer.js
+++ javascript/chronometer.js
@@ -1,34 +1,43 @@
+// ############################
+// ## Iteration 1: The logic ##
+// ############################
+
 class Chronometer {
   constructor() {
-    // ... your code goes here
+    this.currentTime = 0;
+    this.intervalId = null;
   }
 
   start(callback) {
-    // ... your code goes here
+    this.intervalId = setInterval(() => {
+      this.currentTime += 1;
+    }, 1000);
   }
 
   getMinutes() {
-    // ... your code goes here
+    return Math.floor(this.currentTime / 60);
   }
 
   getSeconds() {
-    // ... your code goes here
+    return this.currentTime % 60;
   }
 
   computeTwoDigitNumber(value) {
-    // ... your code goes here
+    return (value * 0.01).toFixed(2).split('.')[1];
   }
 
   stop() {
-    // ... your code goes here
+    clearInterval(this.intervalId);
   }
 
   reset() {
-    // ... your code goes here
+    this.currentTime = 0;
   }
 
   split() {
-    // ... your code goes here
+    let mins = this.computeTwoDigitNumber(this.getMinutes());
+    let secs = this.computeTwoDigitNumber(this.getSeconds());
+    return `${mins}:${secs}`;
   }
 }
 
